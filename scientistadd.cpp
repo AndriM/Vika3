57d367cd781f4e9fa4a424d9158e1c4dc0553065
steinarmh14 <steinarmarinohilmarsson@gmail.com>
2014-12-16 16:19:27 +0000

    All I want for christmas is a hot celebrity body

--- scientistadd.cpp
+++ scientistadd.cpp
@@ -20,17 +20,26 @@ Scientist scientistadd::getScientist()
 
 void scientistadd::on_nameLine_textChanged(const QString &arg1)
 {
-    newScientist.name = ui->nameLine->text().toStdString();
+    if(nameLineValid())
+    {
+        newScientist.name = ui->nameLine->text().toStdString();
+    }
 }
 
 void scientistadd::on_byLine_textChanged(const QString &arg1)
 {
-    newScientist.dateOfBirth = ui->byLine->text().toStdString();
+    if(yearOfBirthValid())
+    {
+        newScientist.dateOfBirth = ui->byLine->text().toStdString();
+    }
 }
 
 void scientistadd::on_dyLine_textChanged(const QString &arg1)
 {
-    newScientist.dateOfDeath = ui->dyLine->text().toStdString();
+    if(deathYearValid())
+    {
+        newScientist.dateOfDeath = ui->dyLine->text().toStdString();
+    }
 }
 
 void scientistadd::on_maleRadio_toggled(bool checked)
@@ -63,17 +72,18 @@ void scientistadd::on_checkBox_toggled(bool checked)
 
 void scientistadd::on_addButton_clicked()
 {
+    if(nameLineValid() && yearOfBirthValid() && deathYearValid())
+    {
     Scientist newScientist = getScientist();
     scienceService.addScientist(newScientist);
     close();
+    }
 }
 
 void scientistadd::on_backButton_clicked()
 {
     close();
 }
-
-
 void scientistadd::on_photoButton_clicked()
 {
     QString filename = QFileDialog::getOpenFileName(this,
@@ -87,3 +97,56 @@ void scientistadd::on_photoButton_clicked()
     newScientist.imagePath = ui->imageLine->text().toStdString();
 
 }
+bool scientistadd::nameLineValid()
+{
+    
+    if(ui->nameLine->text().isEmpty())
+    {
+        QMessageBox::warning(this,"Error input.", " Name line is empty!");
+        return false;
+    }
+   /* if(ui->nameLine->text().isDigit())
+    {
+        QMessageBox::warning(this,"Error input.", " Name line cannot contain a digit");
+        return false;
+    }*/
+    
+    return true;
+}
+bool scientistadd::yearOfBirthValid()
+{
+    
+    if(ui->byLine->text().isEmpty())
+    {
+        QMessageBox::warning(this,"Error input.", " Birth year is empty!");
+        return false;
+    }
+    /*if(ui->byLine->text().isAlpha())
+    {
+        QMessageBox::warning(this,"Error input.", " Birth year cannot contain a alpha");
+        return false;
+    }*/
+    
+    return true;
+    
+}
+bool scientistadd::deathYearValid()
+{
+    
+    if(ui->dyLine->text().isEmpty())
+    {
+        QMessageBox::warning(this,"Error input.", " Death year is empty!");
+        return false;
+    }
+    /*if(ui->dyLine->text().isAlpha())
+    {
+        QMessageBox::warning(this,"Error input.", " Death year cannot contain a alpha");
+        return false;
+    }*/
+    
+    return true;
+}
+/*bool genderValid()
+{
+    
+}*/
--- scientistadd.h
+++ scientistadd.h
@@ -5,6 +5,10 @@
 #include <QFileDialog>
 #include "scientist.h"
 #include "scienceservice.h"
+#include <QMessageBox>
+#include <ctype.h>
+#include <stdio.h>
+#include <cctype>
 
 namespace Ui {
 class scientistadd;
@@ -18,6 +22,10 @@ public:
     explicit scientistadd(QWidget *parent = 0);
     ~scientistadd();
     Scientist getScientist();
+    bool nameLineValid();
+    bool yearOfBirthValid();
+    bool deathYearValid();
+//    bool genderValid();
 private slots:
     void on_nameLine_textChanged(const QString &arg1);
     void on_byLine_textChanged(const QString &arg1);
